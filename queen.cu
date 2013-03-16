9cf5e6b54f53d51430b4b55c5f481d091a062bf1
mamenyaka <mamenyaka@gmail.com>
2013-03-17 00:03:21 +0100

    Optimized code

--- queen.cu
+++ queen.cu
@@ -2,28 +2,10 @@
 
 #define QUEENS 10
 
-__global__
-void queen(int *db, const int n)
+__device__
+void kiir(char *A, int *db)
 {
-    int A[QUEENS];
-
-    A[0] = threadIdx.x;
-    A[1] = threadIdx.y;
-    A[2] = threadIdx.z;
-    A[3] = blockIdx.x/10;
-    A[4] = blockIdx.x%10;
-    A[5] = blockIdx.y/10;
-    A[6] = blockIdx.y%10;
-    A[7] = blockIdx.z/10;
-    A[8] = blockIdx.z%10;
-    A[9] = n;
-
-    for(int i = 0; i < QUEENS; i++)
-        for(int j = 0; j < QUEENS; j++)
-            if((i != j) && (A[i] == A[j] || abs(i - j) == abs(A[i] - A[j])))
-                return;
-
-    char s[256];
+    char s[QUEENS*21 + 1];
     int k = 0;
 
     for(int i = 0; i < QUEENS; i++)
@@ -46,6 +28,45 @@ void queen(int *db, const int n)
     printf("%d.\n%s\n", *db, s);
 }
 
+__global__
+void queen(int *db, const int n)
+{
+    char A[QUEENS];
+    
+    __syncthreads();
+
+    A[0] = threadIdx.x;
+    A[1] = threadIdx.y;
+    A[2] = threadIdx.z;
+    A[3] = blockIdx.x/10;
+    A[4] = blockIdx.x%10;
+    A[5] = blockIdx.y/10;
+    A[6] = blockIdx.y%10;
+    A[7] = blockIdx.z/10;
+    A[8] = blockIdx.z%10;
+    A[9] = n;
+    
+    {
+	bool B[QUEENS];
+	
+	for(int i = 0; i < QUEENS; i++)
+	    B[i] = 0;
+	
+	for(int i = 0; i < QUEENS; i++)
+	    if(!B[A[i]])
+		B[A[i]] = 1;
+	    else
+		return;
+    }
+
+    for(int i = 0; i < QUEENS - 1; i++)
+        for(int j = i + 1; j < QUEENS; j++)
+            if(abs(i - j) == abs(A[i] - A[j]))
+                return;
+    
+    kiir(A, db);
+}
+
 int
 main()
 {
@@ -55,12 +76,9 @@ main()
 
     dim3 blocksPerGrid(100, 100, 100);
     dim3 threadsPerBlock(10, 10, 10);
-
+    
     for(int i = 0; i < QUEENS; i++)
-    {
-        queen<<<blocksPerGrid, threadsPerBlock>>>(d, i);
-        cudaThreadSynchronize();
-    }
+	queen<<<blocksPerGrid, threadsPerBlock>>>(d, i);
 
     cudaMemcpy(&h, d, sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(d);
